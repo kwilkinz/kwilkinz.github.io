68a4d53a16943a483126392f028458fda72a887c
Keri Wilkins <keriannwilkins@gmail.com>
2022-10-28 15:46:28 -0500

    .

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,27 +1,11 @@
 const navToggle = document.querySelector('.nav-toggle');
 const navLinks = document.querySelectorAll('.nav__link');
 
-// Not working at the moment with Github
-
-// document.querySelector(document).ready(function(){
-//     document.querySelector(".project-filter li").click(function(){
-//         document.querySelector(this).classList.add("project-filter-active").siblings().classList.remove("project-filter-active");
-//     })
-// })\
 
 
-// document.querySelector(document).ready(function(){
-//     document.querySelector(".list").click(function(){
-//         const value = document.querySelector(this).attr("data-filter");
-//         if(value == "all"){
-//             document.querySelector(".project-box").show("1000");
-//         } else {
-//             document.querySelector(".project-box").not("."+value).hide("1000");
-//             document.querySelector(".project-box").filter("."+value).show("1000");
-//         }
-//     })
-// })
 
+
+// TODO Convert janilla js to Jquery 
 
 // 1. Function on click toggle the nav bar to open 
 navToggle.addEventListener('click', () => {             //when clicked complete function
